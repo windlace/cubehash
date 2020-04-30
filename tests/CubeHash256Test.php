1d6b8d8f60a3d85172a8d7c8f878f417725d6160
windlace <44790541+windlace@users.noreply.github.com>
2020-04-30 03:16:45 +0200

    Update CubeHash256Test.php

--- tests/CubeHash256Test.php
+++ tests/CubeHash256Test.php
@@ -23,7 +23,7 @@ class CubeHash256Test extends TestCase
             ],
             CubeHash256::iv(8, 1, 256)
         );
-        $this->assertEquals('38d1e8a22d7baac6fd5262d83de89cacf784a02caa866335299987722aeabc59', cubehash256(''));
-        $this->assertEquals('692638db57760867326f851bd2376533f37b640bd47a0ddc607a9456b692f70f', cubehash256('Hello'));
+        $this->assertEquals('38d1e8a22d7baac6fd5262d83de89cacf784a02caa866335299987722aeabc59', cubehash256(8, ''));
+        $this->assertEquals('692638db57760867326f851bd2376533f37b640bd47a0ddc607a9456b692f70f', cubehash256(8, 'Hello'));
     }
 }

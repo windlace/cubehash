468fdb9cfbd0a3d8715ab8eebce1c16efc9cb7b8
windlace <44790541+windlace@users.noreply.github.com>
2020-04-30 02:15:33 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -8,3 +8,11 @@ function cubehash256($string)
 {
     return CubeHash256::hash($string);
 }
+
+// returns 32-bit representation on 64-bit integer
+function i32($value)
+{
+    $value = ($value & 0xFFFFFFFF);
+    if ($value & 0x80000000) $value = -((~$value & 0xFFFFFFFF) + 1);
+    return $value;
+}
